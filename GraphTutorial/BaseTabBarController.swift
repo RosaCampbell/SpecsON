0a1d983127a09ead26718a4fec38ff029951a243
RosaCampbell <roseannacampbell@outlook.com>
2020-09-02 09:04:44 +1200

    Added BaseTabBarController.swift and assigned the TabBarController to this class

---
+++ GraphTutorial/BaseTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseTabBarController.swift
+//  GraphTutorial
+//
+//  Created by Rosa Campbell on 2/09/20.
+//  Copyright © 2020 Campbell. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
