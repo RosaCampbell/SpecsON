b77915707c1ebf6c4c756c4a7a0243e2e1f063ff
RosaCampbell <roseannacampbell@outlook.com>
2020-09-02 11:37:19 +1200

    Set up line chart in Summary and Day of total data and BarChart in week of first seven data points

--- GraphTutorial/BaseTabBarController.swift
+++ GraphTutorial/BaseTabBarController.swift
@@ -7,11 +7,44 @@
 //
 
 import UIKit
+import MSGraphClientModels
 
 class BaseTabBarController: UITabBarController {
+    
+    private let spinner = SpinnerViewController()
+    private var fileContents: Data?
+    private var url: String?
+    var fileData = [[String:String]]()
+    var csvFile: MSGraphDriveItem?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("BaseTabBarController viewDidLoad")
+        print("BaseTabBarController spinner.start")
+        GraphManager.instance.getFileURL(fileId: csvFile?.entityId ?? "") { (fileContentsData: Data?, fileURL: String?, error: Error?) in
+            print("BaseTabBarController: GraphManager.instance.getFileURL")
+            guard let fileContents = fileContentsData, error == nil else {
+                // Show the error
+                let alert = UIAlertController(title: "Error getting file contents",
+                                          message: error.debugDescription,
+                                          preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true)
+                return
+            }
+            self.fileContents = fileContents
 
+            guard let UnwrappedUrl = fileURL, error == nil else {
+                // Show the error
+                let alert = UIAlertController(title: "Error getting file URL", message: error.debugDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true)
+                return
+            }
+            self.url = UnwrappedUrl
+            
+            let csv = csvManager()
+            self.fileData = csv.convertCSV(stringData: csv.readStringFromURL(stringURL: UnwrappedUrl), stringFileName: self.csvFile?.name ?? "nil")
+        }
     }
 }
--- GraphTutorial/WeekViewController.swift
+++ GraphTutorial/WeekViewController.swift
@@ -12,12 +12,13 @@ import Charts
 
 class WeekViewController: UIViewController, ChartViewDelegate {
     
-    private var dayLineChart = LineChartView()
+    private var weekBarChart = BarChartView()
     private var importedFileData = [[String:String]]()
+    private var csvFile: MSGraphDriveItem?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dayLineChart.delegate = self
+        weekBarChart.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -25,34 +26,51 @@ class WeekViewController: UIViewController, ChartViewDelegate {
         DispatchQueue.main.async {
             let tabBar = self.tabBarController as! BaseTabBarController
             self.importedFileData = tabBar.fileData
+            self.csvFile = tabBar.csvFile
             
             // Convert all data to days
             
             
-            self.dayLineChart.frame = CGRect(x: 0, y: 20, width: self.view.frame.size.width - 20, height: self.view.frame.size.height - 40)
-            self.dayLineChart.center = self.view.center
-            self.view.addSubview(self.dayLineChart)
+            self.weekBarChart.frame = CGRect(x: 100, y: 20, width: self.view.frame.size.width - 20, height: self.view.frame.size.height - 300)
+            self.weekBarChart.center = self.view.center
+            self.view.addSubview(self.weekBarChart)
 
-            var entries = [ChartDataEntry]()
+            var entries = [BarChartDataEntry]()
 
             if !self.importedFileData.isEmpty {
-                for index in 1..<self.importedFileData.count {
+                for index in 1..<8 {
                     let datapoint = self.importedFileData[index]
                     if let objTemp = Double(datapoint["Obj"]!) {
-                        entries.append(ChartDataEntry(x: Double(index), y: objTemp))
+                        entries.append(BarChartDataEntry(x: Double(index), y: objTemp))
                     }
                 }
             } else {
                 for i in 1..<10 {
-                    entries.append(ChartDataEntry(x: Double(i), y: 25.00))
+                    entries.append(BarChartDataEntry(x: Double(i), y: 25.00))
                 }
             }
 
-            let set = LineChartDataSet(entries: entries)
+            let set = BarChartDataSet(entries: entries)
             set.colors = ChartColorTemplates.pastel()
-            set.drawCirclesEnabled = false
-            let data = LineChartData(dataSet: set)
-            self.dayLineChart.data = data
+            let data = BarChartData(dataSet: set)
+            self.weekBarChart.data = data
         }
+    }
+    
+    func chartDataPoints(from xYValues: [Date: Double]) -> [ChartDataEntry] {
+      assert(!xYValues.isEmpty)
+      var dataPoints: [ChartDataEntry] = []
+      var startDate = xYValues.keys.sorted(by: <).first!
+      let endDate = xYValues.keys.sorted(by: >).first!
+
+      repeat {
+        guard let yValue = xYValues[startDate] else {
+          startDate = startDate.advanced(by: TimeInterval(24 * 60 * 60))
+          continue
+        }
+        dataPoints.append(ChartDataEntry(x: Double(startDate.timeIntervalSince1970), y: yValue))
+        startDate = startDate.advanced(by: TimeInterval(24 * 60 * 60))
+      } while startDate <= endDate
+      return dataPoints
     }
 }
