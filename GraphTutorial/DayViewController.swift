1d45beb7e62eefef7893061d5cd8eb500637516e
RosaCampbell <roseannacampbell@outlook.com>
2020-09-07 13:19:36 +1200

    Forward one day button works

--- GraphTutorial/DayViewController.swift
+++ GraphTutorial/DayViewController.swift
@@ -15,8 +15,19 @@ class DayViewController: UIViewController, ChartViewDelegate {
     private var dayBarChart = BarChartView()
     private var importedFileData = [[String:String]]()
     private var csvFile: MSGraphDriveItem?
+    private var day: Int = 1
     
     @IBOutlet weak var displayDate: UILabel!
+    
+    @IBAction func forwardOneDay() {
+        if day >= 1 {
+            day += 1
+        }
+    }
+    
+    @IBAction func backOneDay() {
+        day -= 1
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,15 +73,26 @@ class DayViewController: UIViewController, ChartViewDelegate {
     }
     
     func getDataForDay()-> [[String:String]] {
-        var startDayFound = false
-        var i = 0
+        var startDayFound = 0
+        var i = 1
         var dayData = [[String:String]]()
         
-        while !startDayFound {
-            startDayFound = self.importedFileData[i]["Date Time"]!.contains(" 00:0")
+        while startDayFound != day {
+            var isSameDay = false
+            
+            let preDay = self.importedFileData[i]["Date Time"]!.components(separatedBy: " 00:")[0]
+            let currentDay = self.importedFileData[i-1]["Date Time"]!.components(separatedBy: " 00:")[0]
+            
+            if preDay == currentDay {
+                isSameDay = true
+            }
+             
+            if (self.importedFileData[i]["Date Time"]!.contains(" 00:0") && !isSameDay) {
+                startDayFound += 1
+            }
             i += 1
         }
-        
+        print("i = \(i)")
         for j in 0..<288 {
             dayData.append(self.importedFileData[i-1+j])
             //print("oneDaysData: \(dayData[j])")
@@ -87,12 +109,10 @@ class DayViewController: UIViewController, ChartViewDelegate {
             average = 0
             for j in 0..<12 {
                 if let state = dayData[(i*12)+j]["State"] {
-                    print("State at \(String(describing: dayData[(i*12)+j]["Date Time"])) = \(state)")
                     average += Double(state) ?? 0
                 }
             }
             hourlyStateAverages.append(average/12)
-            print("State average for \(i):00 = \(hourlyStateAverages[i])")
         }
         return hourlyStateAverages
     }
